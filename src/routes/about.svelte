e2de67c0674c77455d21d936f90e01975ca9a60d
haikelz <77146709+haikelz@users.noreply.github.com>
2022-02-09 19:29:43 +0700

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,6 +5,5 @@
 <div class="container">
   <h1>About</h1>
   <p>Hai, Saya Haikel. Biasa dipanggil "Kel" atau "Ekel". Saat ini tinggal di Bangka Belitung.</p>
-  <p>Sekitar 4 tahun yang lalu, saya adalah seorang automotive enthusiast yang cukup senang ngoprek motor(walau jurusan saya ga ada kaitannya dengan hal tersebut). Tapi karena beberapa alasan, saya banting setir ke dunia Front End Development.</p>
-  <p>Saat waktu luang, saya biasanya bermain catur, menulis artikel disini, dan berolahraga.</p>
+  <p>Ketika waktu luang, saya biasanya bermain catur, menulis artikel disini, dan berolahraga.</p>
 </div>
